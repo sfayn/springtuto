3db825799c261c786ff378140439ce738e4812c1
Soufiane BENSGHIR <soufiane.bensghir@gmail.com>
2023-05-31 12:23:50 +0200

    clean common 01

--- src/main/java/com/neoxia/util/CommonUtil.java
+++ src/main/java/com/neoxia/util/CommonUtil.java
@@ -28,14 +28,13 @@ public  class CommonUtil {
             if(values != null && (values.length==1&&StringUtils.isNotBlank(values[0]))?true:false){
                 for(int i=0;i<values.length;i++){
                     if(values[i] != null && !"".equals(values[i])){
-//							value = new String(values[i].getBytes("ISO-8859-1"),"UTF-8") + ",";
                         value += values[i] + ",";
                     }
                 }
                 if(value != null && !"".equals(value)){
                     value = value.substring(0, value.length()-1);
                 }
-                if(key.equals("keywords")){//关键字特殊查询字符转义
+                if(key.equals("keywords")){
                     value =  value.replace("_", "\\_").replace("%", "\\%");
                 }
                 returnMap.put(key, value);
